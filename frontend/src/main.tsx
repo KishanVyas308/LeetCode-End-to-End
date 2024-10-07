9677032aad6a40cc407f1e5962546f3159ecc0ed
Kishan Vyas <kishanvyas308@gmail.com>
2024-10-07 22:02:27 +0530

    final touch on ui

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import CodeEditor from "./pages/CodeEditor.tsx";
+import { RecoilRoot } from "recoil";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+ 
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+);
